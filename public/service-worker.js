37dddf5d3c111227ac9eab69a64b913de25affe9
Ryan Peffer <ryanpeffer@icloud.com>
2020-08-04 16:16:48 -0400

    Add files via upload

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,17 +1,9 @@
-const FILES_TO_CACHE = [
-    "/", 
-    "/index.html", 
-    "/index.js", 
-    "/styles.css", 
-    "/db.js", 
-    "/icons/icon-192x192.png", 
-    "/icons/icon-512x512.png",
-    "/manifest.webmanifest"
-];
+const FILES_TO_CACHE = ["/", "/index.html", "index.js"];
 
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 
+// install
 self.addEventListener("install", function(evt) {
   evt.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
@@ -23,6 +15,7 @@ self.addEventListener("install", function(evt) {
   self.skipWaiting();
 });
 
+// activate
 self.addEventListener("activate", function(evt) {
   evt.waitUntil(
     caches.keys().then(keyList => {
@@ -40,12 +33,14 @@ self.addEventListener("activate", function(evt) {
   self.clients.claim();
 });
 
+// fetch
 self.addEventListener("fetch", function(evt) {
   if (evt.request.url.includes("/api/")) {
     evt.respondWith(
       caches.open(DATA_CACHE_NAME).then(cache => {
         return fetch(evt.request)
           .then(response => {
+            // If the response was good, clone it and store it in the cache.
             if (response.status === 200) {
               cache.put(evt.request.url, response.clone());
             }
@@ -53,19 +48,11 @@ self.addEventListener("fetch", function(evt) {
             return response;
           })
           .catch(err => {
+            // Network request failed, try to get it from the cache.
             return cache.match(evt.request);
           });
       }).catch(err => console.log(err))
     );
 
     return;
-}
-
-evt.respondWith(
-  caches.open(CACHE_NAME).then(cache => {
-    return cache.match(evt.request).then(response => {
-      return response || fetch(evt.request);
-    });
-  })
-);
-});
+}});
--- server.js
+++ server.js
@@ -2,10 +2,8 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
-var http = require('http');
-var enforce = require('express-sslify');
 
-const PORT = process.env.PORT || 7000;
+var PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -20,11 +18,12 @@ app.use(express.static("public"));
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false,
+  useUnifiedTopology: true
 });
 
 // routes
 app.use(require("./routes/api.js"));
- 
-http.createServer(app).listen(PORT, function() {
-    console.log('Express server listening on port ' + PORT);
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
 });
